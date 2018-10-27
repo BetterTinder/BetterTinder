714ae3d16211059f5cee2228687589167af6339d
Chris <supergrazc@gmail.com>
2018-10-27 18:11:05 -0400

    Putting in the settings
    99 percent sure this works

--- Michael Stuff/Manager.java
+++ Michael Stuff/Manager.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -14,6 +16,7 @@ import javafx.stage.Stage;
 public class Manager extends Application{
 	GridPane rootpane = new GridPane();
 	Profile profile = new Profile(); //This will be global
+	Setting setting = new Setting(); //this is used in profile; Chris
 	Help help = new Help();//This will be global
 	
 	public static void main(String[] args) {
@@ -23,6 +26,11 @@ public class Manager extends Application{
 	
 	public void start(Stage stage) {
 		rootpane.getChildren().add(profile);// This method would be in each button
+		profile.getSettingBtn().setOnAction(new EventHandler<ActionEvent>(){
+				public void handle(ActionEvent event) {
+				rootpane.getChildren().add(setting);
+				}
+		});
 		Scene scene = new Scene(rootpane,400,500);
 		stage.setScene(scene);
 		stage.show();
