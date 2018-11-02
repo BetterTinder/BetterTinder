bce1c21a144a82b39804ac863018fa7f239669f4
Chris <supergrazc@gmail.com>
2018-11-02 16:14:17 -0400

    sorry for calling you meanie
    but you do eat ass

--- Chris/Manager.java
+++ Chris/Manager.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -14,14 +13,12 @@ import javafx.stage.Stage;
  */
 public class Manager extends Application{
 	GridPane rootpane = new GridPane();
-	Button helpb = new Button("Help");	//global help button @Chris
-	Button prof=new Button("Profile");
 	Setting setting = new Setting(); //this is used in profile; Chris
 	Profile profile = new Profile(rootpane,setting); //This will be global
 	Help help = new Help();//This will be global
 	NewAccount newAcc = new NewAccount(rootpane, profile);
 	LoginScreen login = new LoginScreen(rootpane, newAcc, profile);
-	//buttons buttons = new buttons();
+	//buttons buttons = new buttons()
 
 	//hbox.getChildren().addAll(helpButton, button2, button3, button4);
 	
@@ -30,32 +27,36 @@ public class Manager extends Application{
 	}
 	
 	public void start(Stage stage) {
-		helpb.setMinWidth(400);
-		prof.setMinWidth(400);
-		helpb.setOnAction(new EventHandler<ActionEvent>(){ 
-			public void handle(ActionEvent event) {
-			Help helps = new Help();
-			rootpane.getChildren().add(helps);
-			}
-		});
-		prof.setOnAction(new EventHandler<ActionEvent>(){ 
-			public void handle(ActionEvent event) {
-			Setting sett=new Setting();
-			Profile prof=new Profile(rootpane,sett);
-			rootpane.getChildren().add(prof);
-			}
-		});
 		//profile.setHgap(50);
 		//profile.add(buttons,0,500);
 		rootpane.getChildren().add(login);// This method would be in each button
 		//rootpane.getChildren().add(buttons);
-		rootpane.add(helpb, 0,496); //@Chris
-		rootpane.add(prof, 0,497); //@Chris
 		
 		Scene scene = new Scene(rootpane,400,520);
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+public HBox addButtons(GridPane grid1) {
+	HBox btnBox = new HBox(10);
+	Button helpBtn = new Button("Help");	//global help button @Chris
+	Button profBtn=new Button("Profile");
+	helpBtn.setMinWidth(85);
+	profBtn.setMinWidth(85);
+	helpBtn.setOnAction(new EventHandler<ActionEvent>(){ 
+		public void handle(ActionEvent event) {
+		Help helps = new Help();
+		rootpane.getChildren().add(helps);
+		}
+	});
+	profBtn.setOnAction(new EventHandler<ActionEvent>(){ 
+		public void handle(ActionEvent event) {
+		Setting sett=new Setting();
+		Profile prof=new Profile(rootpane,sett);
+		rootpane.getChildren().add(prof);
+		}
+	});
+	return btnBox;
 }
-
+}
 //Add the buttons with the action events
