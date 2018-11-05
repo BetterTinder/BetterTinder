eb3214a7f92f27d830ee13353b392d1bec5ada93
Chris <supergrazc@gmail.com>
2018-11-04 19:20:26 -0500

    this should have been in the other one

--- Chris/Manager.java
+++ Chris/Manager.java
@@ -1,54 +1,61 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 /**
  * 
- * @author Michael Fornito and also Chris did some stuff
+ * @author Chris, Michael, and Danielle
  *
  */
 public class Manager extends Application{
 	GridPane rootpane = new GridPane();
-	Setting setting = new Setting(); //this is used in profile; Chris
-	Profile profile = new Profile(rootpane,setting); //This will be global
-	Help help = new Help();//This will be global
-	NewAccount newAcc = new NewAccount(rootpane, profile);
-	LoginScreen login = new LoginScreen(rootpane, newAcc, profile);
-	//buttons buttons = new buttons()
-
-	//hbox.getChildren().addAll(helpButton, button2, button3, button4);
+	String style = "-fx-background-color: rgba(255, 255, 255, 1);";
+	Setting setting = new Setting(addButtons(rootpane), rootpane); 
 
 	public static void main(String[] args) {
 		launch(args);	
 	}
 
-	public void start(Stage stage) {
-		//profile.setHgap(50);
-		//profile.add(buttons,0,500);
-		rootpane.getChildren().add(login);// This method would be in each button
-		//rootpane.getChildren().add(buttons);
+	public Profile InitalizeProfile() {
+		Profile profile = new Profile(addButtons(rootpane),rootpane); 
+		return profile;
+	}
 
+	public void start(Stage stage) {
+		NewAccount newAcc = new NewAccount(addButtons(rootpane), InitalizeProfile());
+		LoginScreen login = new LoginScreen(rootpane, newAcc, InitalizeProfile());
+		rootpane.getChildren().add(login);
 		Scene scene = new Scene(rootpane,400,520);
 		stage.setScene(scene);
 		stage.show();
 	}
 
+	/**
+	 * 
+	 * @param grid1
+	 * @return hBox
+	 * This method adds the 4 main buttons to the pane which can be grabbed by other panes
+	 */
 	public HBox addButtons(GridPane grid1) {
 		HBox btnBox = new HBox(10);
 		btnBox.setAlignment(Pos.CENTER);
 
+		//Resets the pane every time the method is called by the button
+		grid1.getChildren().clear();
+		grid1.setStyle(style);
+
 		Button helpBtn = new Button("Help");
-		helpButton.setFont(Font.font("Verdana", FontWeight.LIGHT, 10));
-		helpButton.setMinSize(85, 20);
-		helpButton.setOnAction(event -> {
+		helpBtn.setFont(Font.font("Verdana", FontWeight.LIGHT, 10));
+		helpBtn.setMinSize(85, 20);
+		helpBtn.setOnAction(event -> {
 			System.out.println("Help Button");	
 			Help helps = new Help();
-			rootpane.getChildren().add(helps);
+			grid1.getChildren().addAll(helps,addButtons(grid1));
 		});
 
 		Button profileBtn = new Button("My Profile");
@@ -56,18 +63,19 @@ public class Manager extends Application{
 		profileBtn.setMinSize(85, 20);
 		profileBtn.setOnAction(event -> {
 			System.out.println("My Profile");
-			Setting sett = new Setting();
-			Profile prof = new Profile(rootpane,sett);
-			rootpane.getChildren().add(prof);
+			Setting sett = new Setting(addButtons(grid1),grid1);
+			Profile prof = new Profile(addButtons(grid1), grid1);
+			grid1.getChildren().add(prof);
+
 		});
-		
+
 		Button findBtn = new Button("Find");
 		findBtn.setFont(Font.font("Verdana", FontWeight.LIGHT, 10));
 		findBtn.setMinSize(85, 20);
 		findBtn.setOnAction(event -> {
 			System.out.println("Find");
 		});
-		
+
 		Button matchedBtn = new Button("Matched");
 		matchedBtn.setFont(Font.font("Verdana", FontWeight.LIGHT, 10));
 		matchedBtn.setMinSize(85, 20);
@@ -75,9 +83,13 @@ public class Manager extends Application{
 			System.out.println("Matched");
 		});
 
-
-
+		btnBox.getChildren().addAll(helpBtn,profileBtn,findBtn,matchedBtn);
+		btnBox.setTranslateY(380);
+		btnBox.setTranslateX(1);
 		return btnBox;
 	}
+	
+
+
+
 }
-//Add the buttons with the action events
